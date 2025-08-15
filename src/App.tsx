71d8e31031997e2e14f4ca224b3a4fd199cada48
liuzhe <1804319025@qq.com>
2025-08-15 17:08:25 +0800

    feat

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import reactLogo from '@/assets/react.svg';
+
+function App() {
+	return (
+		<div className="title">
+			<img src={reactLogo} alt="" />
+			小哲自制react模板
+		</div>
+	);
+}
+
+export default App;
